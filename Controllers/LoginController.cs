ad7c5fec5e76aa10e157bd7bf4eea89e4c8f8ff6
Frank17128 <cicinho171288@outlook.com>
2025-09-12 21:33:41 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using JovenProgramadorWed.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JovenProgramadorWed.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ValidaUsuario(Usuario usuario)
+        {
+            try
+            {
+                if(usuario.Email == "Francisco@gmail" && usuario.Senha == "1234")
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    TempData["Msgerro"] = "Erro ao buscar dados do usuario";
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JovenProgramadorWed.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
